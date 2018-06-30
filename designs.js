e54de4b074e04bbe45568da71d0a1cf682ea42d5
Uchenna Sylvester Okoro <38369717+okorocode@users.noreply.github.com>
2018-06-30 15:07:18 +0100

    upload

---
+++ designs.js
@@ -0,0 +1,41 @@
+// Select color input
+// Select size input
+
+let height, width, color;
+
+// When size is submitted by the user, call makeGrid()
+
+$('#sizePicker').submit(function(evt) {
+	evt.preventDefault();
+	height = $('#inputHeight').val();
+	width = $('#inputWeight').val();
+	makeGrid(height, width);
+	// setBackground();
+
+})
+
+function makeGrid(a, b) {
+	$('tr').remove();
+// Your code goes here!
+
+	// Creating the Grid
+	for(let row = 1; row <= a; row++) {
+		$('#pixelCanvas').append('<tr id=table' + row + '></tr>');
+		for(let cell = 1; cell <= b; cell++) {
+			$('#table' + row).append('<td></td>');
+		}
+	}
+
+	// Add color to cell
+	$('td').click(function addColor() {
+		color = $('#colorPicker').val();
+
+		if($(this).attr('style')) {
+			$(this).removeAttr('style');
+		} else {
+			$(this).attr('style', 'background-color:' + color);
+		}
+	})
+
+}
+
